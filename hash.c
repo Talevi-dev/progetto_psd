05be5203530fe11636ea36d3839bd021bdeb435d
Talevi <luigi.talevi1998@gmail.com>
2025-05-15 14:28:10 +0200

    Aggiornamento tabella hash: modificata la struttura dati per permetter l'inserimento di attivita con stesso nome.

--- hash.c
+++ hash.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 #include "attivita.h"
 #include "funzioni_attivita.h"
@@ -18,9 +17,13 @@ struct hash{
     struct node** tavola;
 };
 
+int ottieni_num_elem(tabella_hash ht){
+    return ht->num_elem;
+}
+
 tabella_hash nuova_hash(){
     tabella_hash ht = (tabella_hash)malloc(sizeof(struct hash));
-    if (ht == NULL) {
+    if (ht == NULL){
         printf("Errore allocazione tabella hash.\n");
         exit(EXIT_FAILURE);
     }
@@ -33,25 +36,24 @@ tabella_hash nuova_hash(){
         printf("Errore allocazione di memoria tabella_hash\n");
         exit(EXIT_FAILURE);
     }
-    
-    for (int i = 0; i < ht->dimensione; i++) {
+
+    for (int i = 0; i < ht->dimensione; i++){
         ht->tavola[i] = NULL;
     }
 
     return ht;
 }
 
-
-int hash_fun(tabella_hash ht, char* key){
+int hash_fun(tabella_hash ht, char* nome){
     int idx;
     int sum = 0, factor = 31;
-    for (int i = 0; i < strlen(key); i++) {
+    for (int i = 0; i < strlen(nome); i++){
 
         // sum = sum + (ascii value of
         // char * (primeNumber ^ x))...
         // where x = 1, 2, 3....n
         sum = ((sum % ht->dimensione)
-               + (((int)key[i]) * factor) % ht->dimensione)
+               + (((int)nome[i]) * factor) % ht->dimensione)
               % ht->dimensione;
 
         // factor = factor * prime
@@ -66,83 +68,88 @@ int hash_fun(tabella_hash ht, char* key){
     return idx;
 }
 
-void insert(tabella_hash ht, attivita elem)
-{
+void inserisci_hash(tabella_hash ht, attivita elem){
     int idx = hash_fun(ht, ottieni_nome(elem));
-    struct node* curr = ht->tavola[idx];
-
-    while (curr != NULL) {
-        if (strcmp(ottieni_nome(curr->elemento), ottieni_nome(elem))){
-            return;
-        }
-        curr = curr->next;
-    }
 
     struct node* nuovo_nodo = (struct node*)malloc(sizeof(struct node));
     if (nuovo_nodo == NULL){
-        printf("Errore allocazione di memoria per nodo hash\n");
+        printf("Errore allocazione nodo\n");
         exit(EXIT_FAILURE);
     }
-    
+
     nuovo_nodo->elemento = elem;
     nuovo_nodo->next = NULL;
-     
 
-    if (ht->tavola[idx] == NULL) {
-        ht->tavola[idx] = nuovo_nodo;
-    } else {
-        nuovo_nodo->next = ht->tavola[idx];
-        ht->tavola[idx] = nuovo_nodo;
-    }
+    nuovo_nodo->next = ht->tavola[idx];
+    ht->tavola[idx] = nuovo_nodo;
+
+    ht->num_elem++;
 }
 
-void cancella(tabella_hash ht, char* key){
-
-    int idx = hash_fun(ht, key);
-
-    struct node* prev = NULL;
+int cerca(tabella_hash ht, char* nome, attivita** out) {
+    int idx = hash_fun(ht, nome);
     struct node* curr = ht->tavola[idx];
 
+    int dimensione = 5;
+    int count = 0;
+
+    *out = malloc(dimensione * sizeof(attivita));
+    if (*out == NULL) {
+        printf("Errore allocazione array di risultati.\n");
+        return 0;
+    }
+
     while (curr != NULL) {
-        if (strcmp(ottieni_nome(curr->elemento), key) == 0){
-
-            if (curr == ht->tavola[idx]) {
-                ht->tavola[idx] = curr->next;
+        if (strcmp(ottieni_nome(curr->elemento), nome) == 0) {
+            if (count >= dimensione) {
+                dimensione *= 2;
+                *out = realloc(*out, dimensione * sizeof(attivita));
+                if (*out == NULL) {
+                    printf("Errore realloc array di risultati.\n");
+                    return count;
+                }
             }
+            (*out)[count++] = curr->elemento;
+        }
+        curr = curr->next;
+    }
 
-            else {
+    return count;
+}
+
+void cancella(tabella_hash ht, attivita da_eliminare){
+
+    int idx = hash_fun(ht, ottieni_nome(da_eliminare));
+    struct node* curr = ht->tavola[idx];
+    struct node* prev = NULL;
+
+    while (curr != NULL) {
+        if (curr->elemento == da_eliminare) {
+            if (prev == NULL) {
+                ht->tavola[idx] = curr->next;
+            } else {
                 prev->next = curr->next;
             }
-            free(curr->elemento);
+
+            free(curr->elemento);  
             free(curr);
+            ht->num_elem--;
             break;
         }
+
         prev = curr;
         curr = curr->next;
     }
-    return;
 }
 
-attivita cerca(tabella_hash ht, char* key){
-    int idx = hash_fun(ht, key);
-    struct node* curr = ht->tavola[idx];
-
-    while (curr != NULL) {
-        if (strcmp(ottieni_nome(curr->elemento), key) == 0){
-            return curr->elemento;
-        }
-        curr = curr->next;
+void distruggi_hash(tabella_hash ht){
+    if (ht == NULL){
+        return;
     }
-
-    return NULL;
-}
-
-void distruggi_hash(tabella_hash ht) {
-    if (ht == NULL) return;
 
     for (int i = 0; i < ht->dimensione; i++) {
         struct node* curr = ht->tavola[i];
-        while (curr != NULL) {
+        while (curr != NULL){
             struct node* temp = curr;
             curr = curr->next;
 
@@ -152,35 +159,34 @@ void distruggi_hash(tabella_hash ht) {
     }
 
     free(ht->tavola);
-
     free(ht);
 }
 
-void stampa_hash(tabella_hash ht, int p){
-    if (ht == NULL) {
-        printf("Tabella hash non inizializzata.\n");
+void stampa_hash(tabella_hash ht, int priorita){
+    if (ht == NULL || ottieni_num_elem(ht) == 0) {
+        printf("Erroe: Tabella hash vuota.\n");
         return;
     }
 
-    if (p >= 0) {
-        printf("Contenuto della tabella hash con priorità %d:\n", p);
+    if (priorita > 0) {
+        printf("Contenuto della tabella hash con priorità %d:\n", priorita);
     } else {
         printf("Contenuto della tabella hash:\n");
     }
 
     for (int i = 0; i < ht->dimensione; i++) {
-        struct node* curr = ht->tavola[i];
+        struct node* currNode = ht->tavola[i];
 
-        if (curr != NULL) {
-            while (curr != NULL) {
-                if (p > 0) {
-                    if (ottieni_priorita(curr->elemento) == p) {
-                        stampa_attivita2(curr->elemento);
+        if (currNode != NULL) {
+            while (currNode != NULL) {
+                if (priorita > 0) {
+                    if (ottieni_priorita(currNode->elemento) == priorita) {
+                        stampa_attivita2(currNode->elemento);
                     }
                 } else {
-                    stampa_attivita2(curr->elemento);
+                    stampa_attivita2(currNode->elemento);
                 }
-                curr = curr->next;
+                currNode = currNode->next;
             }
         }
     }
--- hash.h
+++ hash.h
@@ -1,2 +1,17 @@
 typedef struct hash* tabella_hash;
 
+#include "attivita.h"
+
+tabella_hash nuova_hash();
+
+int hash_fun(tabella_hash ht, char* nome);
+
+void inserisci_hash(tabella_hash ht, attivita elem);
+
+int cerca(tabella_hash ht, char* nome, attivita** out);
+
+void distruggi_hash(tabella_hash ht);
+
+void stampa_hash(tabella_hash ht, int priorita);
+
+void cancella(tabella_hash ht, attivita da_eliminare);
